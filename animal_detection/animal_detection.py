e15a9364b086d41701428e639973550fe473732f
Minakshi Kaushik <minakshikaushik22122005@gmail.com>
2026-06-08 19:48:27 +0530

    Added animal detection project structure and updated gitignore

---
+++ animal_detection/animal_detection.py
@@ -0,0 +1,10 @@
+"""
+Animal Detection Module
+
+Status: Under Development
+
+Waiting for mentor clarification regarding:
+- Separate project vs repository extension
+- Dataset requirements
+- Training requirements
+"""
